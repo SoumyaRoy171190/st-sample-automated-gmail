e3d00e03ec80e35c1c5baef014627bc44a19f44c
Soumya Roy <soumjroy@in.ibm.com>
2017-12-27 17:02:35 +0530

    inital project base

---
+++ src/main/java/sample/automation/st/gmail/com/App.java
@@ -0,0 +1,13 @@
+package sample.automation.st.gmail.com;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
